f2fd329c043c74bd56958818c0d89e3321ac1cd2
Sanzinan Afrose <76949859+sanzinanafrose@users.noreply.github.com>
2024-10-12 13:06:18 +0600

    Delete BMI Calculator.py

--- BMI Calculator.py
+++
@@ -1,7 +0,0 @@
-#BMI = Weight in kgs / (height in m)^2
-
-weight = float(input("Enter weight in kgs: "))
-height = float(input("Enter height in meters: "))
-
-bmi = weight / (height*height)
-print("BMI is:  " + str(bmi))
